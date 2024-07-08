0c00d1d0473b259487b502a0a83b6b7e769b6d02
Rehan Sutar <rehansutar058@gmail.com>
2024-07-08 12:12:30 +0530

    first commit

---
+++ blood_app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from blood_app.models import *
+
+# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Category)
+admin.site.register(Blood_Donation)
+admin.site.register(Order)
+admin.site.register(Appointment)
